6c4bebf9302365121332a613716bc3526f4f757a
hanzzj <45984853+hanzzj@users.noreply.github.com>
2019-01-09 21:13:47 +0800

    Update httpclient.c

--- httpclient.c
+++ httpclient.c
@@ -1,107 +1,108 @@
-#define _TUOBAO_TCP_CLIENT_
-#include <netinet/in.h> 
-#include <sys/socket.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <errno.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-
-typedef struct _tuobao_tcpclient{
-  int socket;
-  int remote_port; 
-  char remote_ip[16]; 
-  struct sockaddr_in _addr;
-  int connected; 
-} tuobao_tcpclient;
-    int tuobao_tcpclient_create(tuobao_tcpclient *,const char *host, int port);
-    int tuobao_tcpclient_conn(tuobao_tcpclient *);
-    int tuobao_tcpclient_recv(tuobao_tcpclient *,char **lpbuff,int size);
-    int tuobao_tcpclient_send(tuobao_tcpclient *,char *buff,int size);
-    int tuobao_tcpclient_close(tuobao_tcpclient *);
-    int http_post(tuobao_tcpclient *pclient,char *page,char *request,char **response){
-char post[300],host[100],content_len[100];
-char *lpbuf,*ptmp;
-int len=0;
-lpbuf = NULL;
-    const char *header2="User-Agent: Tuobao Http 0.1\r\nCache-Control: no-cache\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: */*\r\n";
- sprintf(post,"POST %s HTTP/1.0\r\n",page);
-sprintf(host,"HOST: %s:%d\r\n",pclient->remote_ip,pclient->remote_port);
-sprintf(content_len,"Content-Length: %d\r\n\r\n",strlen(request));
-len = strlen(post)+strlen(host)+strlen(header2)+strlen(content_len)+strlen(request)+1;
- lpbuf = (char*)malloc(len);
- if(lpbuf==NULL){
- return -1;
-}
- strcpy(lpbuf,post);
- strcat(lpbuf,host);
- strcat(lpbuf,header2);
- strcat(lpbuf,content_len);
-strcat(lpbuf,request);
-
- if(!pclient->connected){
- tuobao_tcpclient_conn(pclient);
- }
-if(tuobao_tcpclient_send(pclient,lpbuf,len)<0){
- return -1;
-}
-    printf("发送请求:\n%s\n",lpbuf);
-/*释放内存*/
-if(lpbuf != NULL) free(lpbuf);
-lpbuf = NULL;
-/*it's time to recv from server*/
-if(tuobao_tcpclient_recv(pclient,&lpbuf,0) <= 0){
-if(lpbuf) free(lpbuf);
- return -2;
- }
-printf("接收响应:\n%s\n",lpbuf);
-memset(post,0,sizeof(post));
-strncpy(post,lpbuf+9,3);
-if(atoi(post)!=200){
-if(lpbuf) free(lpbuf);
-return atoi(post);
- }
-
-ptmp = (char*)strstr(lpbuf,"\r\n\r\n");
- if(ptmp == NULL){
- free(lpbuf);
- return -3;
- }
-ptmp += 4;/*跳过\r\n*/
-len = strlen(ptmp)+1;
-*response=(char*)malloc(len);
- if(*response == NULL){
- if(lpbuf) free(lpbuf);
-return -1;
- }
- memset(*response,0,len);
-memcpy(*response,ptmp,len-1);
- /*从头域找到内容长度,如果没有找到则不处理*/
-ptmp = (char*)strstr(lpbuf,"Content-Length:");
-if(ptmp != NULL){
-char *ptmp2;
-ptmp += 15;
-ptmp2 = (char*)strstr(ptmp,"\r\n");
- if(ptmp2 != NULL){
- memset(post,0,sizeof(post));
-strncpy(post,ptmp,ptmp2-ptmp);
-if(atoi(post)<len)
-(*response)[atoi(post)] = '\0';
- }
-}
- if(lpbuf) free(lpbuf);
- return 0;
-}
-int main(){
- tuobao_tcpclient client;
- char *response = NULL;
- printf("开始组包\n");
- tuobao_tcpclient_create(&client,"13.250.177.223",80);
- if(http_post(&client,"/i.php","f1=hello",&response)){
- printf("失败!\n");
- exit(2);
-}
- printf("响应:\n%d:%s\n",strlen(response),response);
-free(response);
- return 0;
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+ 
+#define IPSTR "13.250.177.223" //服务器IP地址;
+#define PORT 80
+#define BUFSIZE 1024
+ 
+int main(int argc, char **argv)
+{
+        int sockfd, ret, i, h;
+        struct sockaddr_in servaddr;
+        char str1[4096], str2[4096], buf[BUFSIZE], *str;
+        socklen_t len;
+        fd_set   t_set1;
+        struct timeval  tv;
+         
+         //创建套接字
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+                printf("创建网络连接失败,本线程即将终止---socket error!\n");
+                exit(0);
+        };
+ 
+        bzero(&servaddr, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(PORT);
+        if (inet_pton(AF_INET, IPSTR, &servaddr.sin_addr) <= 0 ){
+                printf("创建网络连接失败,本线程即将终止--inet_pton error!\n");
+                exit(0);
+        };
+ 
+        if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+                printf("连接到服务器失败,connect error!\n");
+                exit(0);
+        }
+        printf("与远端建立了连接\n");
+        memset(str2, 0, 4096);
+        strcat(str2, "theDataToPost");
+        str=(char *)malloc(128);
+        len = strlen(str2);
+        sprintf(str, "%d", len);
+ 
+        memset(str1, 0, 4096);
+        strcat(str1, "GET /ymnets/p/6255674.html HTTP/1.1\n");
+        strcat(str1, "Host: www.cnblogs.com\n");
+        strcat(str1, "Content-Type: text/html\n");
+        strcat(str1, "Content-Length: ");
+        strcat(str1, str);
+        strcat(str1, "\n\n");
+ 
+        strcat(str1, str2);
+        strcat(str1, "\r\n\r\n");
+        printf("%s\n",str1);
+ 
+        ret = write(sockfd,str1,strlen(str1));
+        if (ret < 0) {
+                printf("发送失败！错误代码是%d，错误信息是'%s'\n",errno, strerror(errno));
+                exit(0);
+        }else{
+                printf("消息发送成功，共发送了%d个字节！\n\n", ret);
+        }
+ 
+        FD_ZERO(&t_set1);
+        FD_SET(sockfd, &t_set1);
+ 
+        while(1){
+                sleep(2);
+                tv.tv_sec= 0;
+                tv.tv_usec= 0;
+                h= 0;
+                printf("--------------->1");
+                h= select(sockfd +1, &t_set1, NULL, NULL, &tv);
+                printf("--------------->2");
+ 
+                //if (h == 0) continue;
+                if (h < 0) {
+                        close(sockfd);
+                        printf("在读取数据报文时SELECT检测到异常，该异常导致线程终止！\n");
+                        return -1;
+                };
+ 
+                if (h > 0){
+                        memset(buf, 0, 4096);
+                        i= read(sockfd, buf, 4095);
+                        if (i==0){
+                                close(sockfd);
+                                printf("读取数据报文时发现远端关闭，该线程终止！\n");
+                                return -1;
+                        }
+ 
+                        printf("%s\n", buf);
+                }
+        }
+        close(sockfd);
+ 
+ 
+        return 0;
 }
